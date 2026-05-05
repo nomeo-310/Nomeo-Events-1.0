9a2b9fa603a643c707d2e7bc70c600f41c832ba9
Salomi Afolabi Onome <onome962@gmail.com>
2026-05-05 18:50:29 +0100

    copnfklict resolved

--- apps/nomeo-events-client/lib/auth-client.ts
+++ apps/nomeo-events-client/lib/auth-client.ts
@@ -4,7 +4,7 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import type { createAuth } from "./auth";
 
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL ?? "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL!,
   plugins: [
     emailOTPClient(),
     inferAdditionalFields<ReturnType<typeof createAuth>>(),
